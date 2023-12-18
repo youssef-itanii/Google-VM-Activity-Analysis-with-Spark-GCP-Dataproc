798622083b739d30722392cbc6a364b89e8d574f
Abbas <AbbasKhreiss0@gmail.com>
2023-12-18 04:07:42 +0100

    Get Class Eviction Probability

--- src/main.py
+++ src/main.py
@@ -176,15 +176,29 @@ def getSchedClassDistribution():
 
 def getClassEvictProbability():
     EVICT = '2'
+
     start = time.time()
-    event_type_index_task_event = schema.getFieldNoByContent("task_events", "event type")
-    sched_class_index_task_events = schema.getFieldNoByContent("task_events" , "scheduling class")
+    event_type_index = schema.getFieldNoByContent("task_events", "event type")
+    scheduling_class_index = schema.getFieldNoByContent("task_events" , "scheduling class")
+    task_index_index = schema.getFieldNoByContent("task_events" , "task index")
+    job_id_index = schema.getFieldNoByContent("task_events" , "job ID") 
+    
+    # note that a task could change its scheduling class
+    # map (job_id,task_index,scheduling_class) -> (event_type)
+    class_and_event_type_per_task = task_events.map(lambda e: ((e[job_id_index],e[task_index_index],e[scheduling_class_index]),(e[event_type_index])))
+    
+    # filter where (job_id,task_index,scheduling_class) -> (EVICT)
+    evictions_per_task = class_and_event_type_per_task.filter(lambda x: x[1][0] == EVICT).groupByKey()
+    
+    # get number of tasks evicted per class (scheduling_class -> tasks evicted)
+    evicted_tasks_per_class = evictions_per_task.map(lambda pr : (int(pr[0][2]) , 1)).reduceByKey(lambda x,y:x+y).collect()
+    evicted_tasks_per_class = dict(evicted_tasks_per_class)
+    
+    # get number of tasks per class 
+    total_tasks_per_class = class_and_event_type_per_task.map(lambda x: (int(x[0][2])  , 1)).reduceByKey(lambda x,y : x+y)
 
-    task_sched_evitctions = task_events.filter(lambda x: x[event_type_index_task_event] == EVICT).map(lambda x: (int(x[sched_class_index_task_events]) , 1)).reduceByKey(lambda x,y:x+y).collect()
-    task_sched_evitctions = (dict(task_sched_evitctions))
-    total_tasks_per_class = task_events.map(lambda x: (int(x[event_type_index_task_event])  , 1)).reduceByKey(lambda x,y : x+y)
-
-    eviction_probability = total_tasks_per_class.map(lambda x: (x[0] , round(task_sched_evitctions[x[0]]/x[1],4) ) if x[0] in task_sched_evitctions else (x[0] , 0)).collect()
+    # get the probability that is evcited per class (num_evicted_tasks/total_num_of_tasks)
+    eviction_probability = total_tasks_per_class.map(lambda x: (x[0] , round(evicted_tasks_per_class[x[0]]/x[1],4) ) if x[0] in evicted_tasks_per_class else (x[0] , 0)).sortByKey().collect()
     appendToOutput("Class_Eviction_Probabiltiy" , time.time() - start  , eviction_probability)
 
 
